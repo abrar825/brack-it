2a340bfb8794a47bb2e938dd447cfcbed77d3d22
abrar <abrar825@gmail.com>
2023-10-29 00:32:43 -0400

    Pick submission page modifications

--- src/main/java/com/amc/bracketup/BracketupApplication.java
+++ src/main/java/com/amc/bracketup/BracketupApplication.java
@@ -8,11 +8,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -74,17 +79,31 @@ public class BracketupApplication {
             System.out.println(responseB);
 
 
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        String connectionString = "mongodb+srv://abrar:dhaka444@atlascluster.zsmesm4.mongodb.net/?retryWrites=true&w=majority";
+        ServerApi serverApi = ServerApi.builder()
+                .version(
+                        ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-
-
-
 
 
 }
--- src/main/java/com/amc/bracketup/controller/PlayerController.java
+++ src/main/java/com/amc/bracketup/controller/PlayerController.java
@@ -2,13 +2,11 @@ package com.amc.bracketup.controller;
 
 import com.amc.bracketup.entity.Player;
 import com.amc.bracketup.service.PlayerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/players")
@@ -20,5 +18,10 @@ public class PlayerController {
     public ResponseEntity<Player> addPlayer(@RequestBody Player player) {
         Player addedPlayer = playerService.addPlayer(player);
         return new ResponseEntity<>(addedPlayer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get")
+    public String getPlayer() {
+        return "Player!";
     }
 }
